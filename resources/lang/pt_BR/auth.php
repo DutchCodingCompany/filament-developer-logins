6ec14da9476dd397301d8b976b3a02b4471246ee
Patrício Martins <eupatriciomartins@gmail.com>
2024-08-01 09:48:42 -0300

    Adding Brazilian Portuguese translation

---
+++ resources/lang/pt_BR/auth.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'login-as' => 'Entrar como',
+    'switch-to' => 'Mudar para',
+
+    'messages' => [
+        'failed' => 'Falha no login, verifique se o usuário tem permissão para acessar o painel.',
+    ],
+];
