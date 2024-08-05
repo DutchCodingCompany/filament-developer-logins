5584784bc18216629aa555bd76f2120e98ea0d5f
Olivtradet <deoliveira.elias.eus@gmail.com>
2024-08-05 14:40:29 +0200

    Add Spanish translation

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'login-as' => 'Iniciar sesión como ',
+    'switch-to' => 'Cambiar a',
+
+    'messages' => [
+        'failed' => 'Login fallido, compruebe si el usuario tiene permiso para acceder al panel.',
+    ],
+];
