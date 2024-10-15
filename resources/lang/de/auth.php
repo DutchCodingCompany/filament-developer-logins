49adf5afc8defec4a696916e03bf1242c92143f3
grafst <grafst@users.noreply.github.com>
2024-10-15 09:37:26 +0200

    translate to german

---
+++ resources/lang/de/auth.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'login-as' => 'Einloggen als',
+    'switch-to' => 'Wechseln zu',
+
+    'messages' => [
+	    'failed' => 'Login gescheitert, überprüfen Sie ob der Benutzer Rechte für das Panel hat.'
+    ],
+];
