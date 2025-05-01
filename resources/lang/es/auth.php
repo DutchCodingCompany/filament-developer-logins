dc74108f79335300a67b26bff4bcf941d3599bce
Jaime Sares <jaimesares@gmail.com>
2025-05-01 10:41:46 +0200

    feat: add spanish translation

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'login-as' => 'Iniciar sesión como',
+    'switch-to' => 'Cambiar a',
+
+    'messages' => [
+        'failed' => 'Error al iniciar sesión, verifica si el usuario tiene permiso para acceder al panel.',
+        'failed_not_found' => 'Error al iniciar sesión, no se pudo encontrar al usuario.',
+    ],
+];
