f0c305c3beba2031559608e33c30ebea2cd18db2
Bram Raaijmakers <b.raaijmakers@hotmail.com>
2025-08-12 16:04:38 +0200

    Fix CS

--- src/FilamentDeveloperLoginsPlugin.php
+++ src/FilamentDeveloperLoginsPlugin.php
@@ -7,8 +7,8 @@ use Closure;
 use DutchCodingCompany\FilamentDeveloperLogins\Exceptions\ImplementationException;
 use Filament\Contracts\Plugin;
 use Filament\Facades\Filament;
-use Filament\Schemas\Concerns\HasColumns;
 use Filament\Panel;
+use Filament\Schemas\Concerns\HasColumns;
 use Filament\Support\Concerns\EvaluatesClosures;
 
 class FilamentDeveloperLoginsPlugin implements Plugin
