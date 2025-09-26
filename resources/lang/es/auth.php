641ffa01e13436d725cabccff92697b5d43e7ee0
Germán Boquizo Sánchez <xekevara@gmail.com>
2025-09-27 00:04:02 +0200

    feat: add Spanish support

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'login-as' => 'Iniciar sesión como',
+    'switch-to' => 'Cambiar a',
+
+    'messages' => [
+        'failed' => 'Error al iniciar sesión, verifica si el usuario tiene acceso al panel.',
+        'failed_not_found' => 'Error al iniciar sesión, no se puede encontrar al usuario.',
+    ],
+];
