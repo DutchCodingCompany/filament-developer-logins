38309a1f402ec53e75b1c175e3ca8e76814e514e
anindyoy <anindyo.yudhistiro@gmail.com>
2026-08-26 20:12:02 +0700

    feat: add Indonesian (id) translation

---
+++ resources/lang/id/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'login-as' => 'Masuk sebagai',
+    'switch-to' => 'Beralih ke',
+
+    'messages' => [
+        'failed' => 'Gagal masuk, periksa apakah pengguna diizinkan mengakses panel.',
+        'failed_not_found' => 'Gagal masuk, pengguna tidak ditemukan.',
+    ],
+];
